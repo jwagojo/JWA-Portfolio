e6f17cb8739079f9beaf1937512a67d13815d9c3
Wesley Agojo <164763036+jwagojo@users.noreply.github.com>
2026-03-16 16:40:11 -0400

    Update ASCIIText to replace name with placeholder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
                     </div>
                     <div className={`absolute w-full h-[300px] top-[30%] left-1/2 -translate-x-1/2 z-[2] ${isTransitioning ? "fade-out" : "fade-in-ASCII"}`}>
                         <ASCIIText 
-                            text="I'm Royce Chud Roxas"
+                            text="I'm Johann *******"
                             enableWaves={false}
                             asciiFontSize={5}
                             textFontSize={200}
@@ -67,7 +67,7 @@ function Home() {
                     </div>
                     <div className={`absolute w-full h-[175px] md:h-[300px] top-[45%] md:top-[35%] left-1/2 -translate-x-1/2 z-[2] px-1 ${isTransitioning ? "fade-out" : "fade-in-ASCII"}`}>
                         <ASCIIText 
-                            text="Royce Chud Roxas"
+                            text="Johann *******"
                             enableWaves={false}
                             asciiFontSize={isMobile ? 2 : 5}
                         />
